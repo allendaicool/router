29244f849ed5ef78a3094bfc6333452c38f288ca
Keenon Werling <keenon@stanford.edu>
2013-12-07 21:36:58 -0800

    no message

--- router/sr_nat.c
+++ router/sr_nat.c
@@ -117,7 +117,7 @@ uint16_t cksum_tcp(sr_ip_hdr_t* ip_hdr, sr_tcp_hdr_t* tcp_hdr, uint16_t len) {
     psuedo->ip_src = ip_hdr->ip_src;
     psuedo->ip_dst = ip_hdr->ip_dst;
     psuedo->ip_tos = ip_hdr->ip_tos;
-    psuedo->tcp_len = htons(len);
+    psuedo->tcp_len = len;
 
     uint16_t cksum_val = cksum(blob,bloblen);
     free(blob);
